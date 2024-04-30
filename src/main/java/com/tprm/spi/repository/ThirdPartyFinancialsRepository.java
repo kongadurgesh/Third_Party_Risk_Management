b4ff9b589c76317e051c629711221930e2b54310
Durgesh Konga <kongadurgesh20@gmail.com>
2024-04-30 23:40:52 +0530

    added implementation for revenue range filter

--- src/main/java/com/tprm/spi/repository/ThirdPartyFinancialsRepository.java
+++ src/main/java/com/tprm/spi/repository/ThirdPartyFinancialsRepository.java
@@ -1,5 +1,11 @@
 package com.tprm.spi.repository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +13,20 @@ import com.tprm.spi.entity.ThirdPartyFinancials;
 
 @Repository
 public interface ThirdPartyFinancialsRepository extends MongoRepository<ThirdPartyFinancials, String> {
+
+    default List<String> getThirdPartyFinancialIdsForRevenueRange(Double fromRange, Double toRange,
+            MongoTemplate mongoTemplate) {
+        Query query = new Query();
+        if (fromRange != null && toRange != null)
+            query.addCriteria(Criteria.where("revenue").gte(fromRange).lte(toRange));
+        else if (fromRange != null)
+            query.addCriteria(Criteria.where("revenue").gte(fromRange));
+        else if (toRange != null)
+            query.addCriteria(Criteria.where("revenue").lte(toRange));
+        List<ThirdPartyFinancials> thirdPartyFinancials = mongoTemplate.find(query, ThirdPartyFinancials.class);
+        return thirdPartyFinancials.stream()
+                .map(ThirdPartyFinancials::getFinancialID)
+                .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/com/tprm/spi/service/ThirdPartyFinancialsService.java
+++ src/main/java/com/tprm/spi/service/ThirdPartyFinancialsService.java
@@ -1,7 +1,10 @@
 package com.tprm.spi.service;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import com.tprm.spi.dto.ThirdPartyFinancialsDTO;
@@ -14,6 +17,9 @@ public class ThirdPartyFinancialsService {
     private ThirdPartyFinancialsRepository thirdPartyFinancialsRepository;
 
     @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
     private ModelMapper modelMapper;
 
     public ThirdPartyFinancialsDTO saveFinancials(ThirdPartyFinancialsDTO thirdPartyFinancialsDTO) {
@@ -23,6 +29,11 @@ public class ThirdPartyFinancialsService {
 
     public void deleteFinancialsbyId(String id) {
         thirdPartyFinancialsRepository.deleteById(id);
+    }
+
+    public List<String> getThirdPartyFinancialIdsForRevenueRange(Double fromRange, Double toRange) {
+        return thirdPartyFinancialsRepository.getThirdPartyFinancialIdsForRevenueRange(fromRange, toRange,
+                mongoTemplate);
     }
 
     private ThirdPartyFinancials convertToThirdPartyFinancialsEntity(ThirdPartyFinancialsDTO thirdPartyFinancialsDTO) {
